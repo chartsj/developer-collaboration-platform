799f43be233df78af458a071ad191d39b3297634
unknown <charlenetansijie@outlook.com>
2026-03-19 21:54:03 +0800

    frontend

--- backend/flask/app.py
+++ backend/flask/app.py
@@ -1,8 +1,29 @@
 from flask import Flask, request
+import os
+import psycopg2
 
 app = Flask(__name__)
 
-@app.route("/")
+
+def get_db_connection():
+    conn = psycopg2.connect(host='localhost', database='git_project',user='postgres', password = 'charl')
+    return conn
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM issues;')
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    return rows
+
+@app.route('/kik')
+def jko():
+    return {"message":"ok"}
+
+@app.route("/hi")
 def hello_world():
     return "<p>Hello, World!</p>"
 
